fc670660d94759c1a49f6183c02df3984ba7611e
Sivaranjani S S <156828520+SivaranjaniSS06@users.noreply.github.com>
2024-12-31 19:45:30 +0530

    Update Navbar.jsx

--- admin/src/Components/Navbar/Navbar.jsx
+++ admin/src/Components/Navbar/Navbar.jsx
@@ -1,15 +1,25 @@
-import React from 'react'
-import './Navbar.css'
-import nav_logo from '../../assets/logo1.jpeg'
-import navProfile from  '../../assets/nav-profile.svg'
+import React from 'react';
+import './Navbar.css';
+import navlogo from '../../assets/logo1.jpeg';
+import navProfile from '../../assets/profile.jpeg';
+import arrow from '../../assets/dropdown.jpeg'; // Import the arrow image
 
-const Navbar = () =>{
+const Navbar = () => {
   return (
     <div className='navbar'>
-        <img src={nav_logo} alt="" className="nav-logo" />
-        <img src={navProfile} className='nav-profile' alt="" />
+      <div className='nav-left'>
+        <img src={navlogo} alt="Logo" className="nav-logo" />
+        <div className='nav-titles'>
+          <h1 className='nav-main-title'>DECATHLON</h1>
+          <h2 className='nav-sub-title'>ADMIN PANEL</h2>
+        </div>
+      </div>
+      <div className='nav-right'>
+        <img src={navProfile} alt="Profile" className="nav-profile" />
+        <img src={arrow} alt="Arrow" className="nav-arrow" />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
